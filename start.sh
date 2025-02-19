0432173765c428cf05611c391befa30a0d01aaf8
PedroLuizPBR <pedroluizbr@yahoo.com.br>
2025-02-19 08:21:07 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ SETTINGS_FILE="/opt/app-root/src/.node-red/settings.js"
 if [ ! -f "$SETTINGS_FILE" ]; then
     echo "Starting Node-RED on port 3001..."
     exec node-red -p 3001
+    sed -i 's/port: 1880/port: 3001/g' "$SETTINGS_FILE"
 else
     echo "Starting Node-RED..."
     exec node-red
